a72e9b50bd4f2b94e25c69e18c0ae3f8aebcc86c
MatRoyer1000 <mat.royer1000@gmail.com>
2022-08-31 16:12:51 +0200

    zigoooo?

---
+++ GJ_Masseur/Assets/S_Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Timer : MonoBehaviour
+{
+    public GameObject Timer;
+    private Animator AC_Timer;
+    public
+    void Start()
+    {
+       AC_Timer = Timer.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartTimerJ1() {
+        Debug.Log("TimerJ1");
+        AC_Timer.SetTrigger("PlayTimerJ1");     
+    }
+
+    public void StartTimerJ2()
+    {
+        Debug.Log("TimerJ2");
+        AC_Timer.SetTrigger("TimerPlayJ2");
+    }
+
+    public void StartTimer()
+    {
+        AC_Timer.SetTrigger("TimerPlay");
+        Debug.Log("Timer General");
+    }
+}
