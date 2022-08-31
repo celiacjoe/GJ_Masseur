89ab04b7bf5a5e65f1546c08b76f7d338c0b90ab
Louis Cortes <cortes.lou@gmail.com>
2022-08-31 21:00:31 +0200

    flash

--- GJ_Masseur/Assets/PostEffect.cs
+++ GJ_Masseur/Assets/PostEffect.cs
@@ -6,10 +6,15 @@ public class PostEffect : MonoBehaviour
 {
 
     public Material effect;
+    [Range(0, 1)]
+    public float flash;
+    void Update()
+    {
 
-    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    }
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         Graphics.Blit(source, destination, effect);
-    
+        effect.SetFloat("_flash", flash);
     }
 }
